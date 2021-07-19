84adf74ecd10c7b092f0d36945c407129eee2755
aliihsanakcay <alihsanakcayy@gmail.com>
2021-07-19 14:06:03 +0300

    recursive power method program

---
+++ recursivePower/readme.md
@@ -0,0 +1,4 @@
+## Recursive Metot ile Üslü Sayı Hesaplama 
+
+Taban ve üs değerleri kullanıcıdan alınan üs alma işlemini "Recursive" metot kullanarak yapan programdır. 
+
---
+++ recursivePower/src/recursivePower/Main.java
@@ -0,0 +1,29 @@
+package recursivePower;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	static int power(int base, int exponent) {
+		if(exponent == 0) {
+			return 1;
+		}
+		
+		return base * power(base, exponent - 1);
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Taban deðeri giriniz : ");
+		int base = input.nextInt();
+		System.out.print("Üs deðerini giriniz : ");
+		int exponent = input.nextInt();
+		
+		int result = power(base, exponent);
+		System.out.println("Sonuç : " + result);
+		
+		input.close();
+	}
+
+}
